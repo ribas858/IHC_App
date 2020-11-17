2a6197fb606cdc878b868385b334561299a47f8b
ribas858 <ribas858@gmail.com>
2020-11-17 01:19:11 -0300

    save

---
+++ App.js
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <MapView style={styles.mapStyle} />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+});
